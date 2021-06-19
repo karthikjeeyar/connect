e3294f076a1581d4da78c33ff98ad5798c7cf1a7
Karthik <karthik.jk11@gmail.com>
2021-06-19 10:50:49 +0530

    Add Initial layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <div className="app__header__container"></div>
+      </div>
+      <div className="app__workspace-switcher"></div>
+      <div className="app__sidepanel"></div>
+      <div className="app__main-content"></div>
+      <div className="app__reading-pane"></div>
     </div>
   );
 }
